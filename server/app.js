9caa2b67093c788b29453991a7c67ec1d3d49455
Kiran <ravi_kiran_vs@yahoo.co.in>
2015-09-16 08:58:54 +0530

    port from env

--- server/app.js
+++ server/app.js
@@ -16,6 +16,8 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(80, function(){
-  console.log('listening on *:80');
+var port = process.env.PORT || 5000;
+
+http.listen(port, function(){
+  console.log('listening on *:5000');
 });
