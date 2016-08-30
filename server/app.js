f5551eac648e2b0a681ef918868ca51d3ccab779
Kiran Srikanta <Kiran.Srikanta@emc.com>
2016-08-30 11:07:57 +0530

    rollback

--- server/app.js
+++ server/app.js
@@ -1,59 +1,58 @@
 import express from 'express';
 import http from 'http';
-
+import socket from 'socket.io';
 import SillyChatBot from './bot';
 import gzipStatic from 'connect-gzip-static';
-module.exports = function (socket) {
-  const app = express();
-  const httpServer = http.Server(app);
-  const io = socket(httpServer);
-  const chatBot = new SillyChatBot();
 
-  app.use(gzipStatic('public'));
-  //app.use(express.static('public'));
+const app = express();
+const httpServer = http.Server(app);
+const io = socket(httpServer);
+const chatBot = new SillyChatBot();
 
-  let users = {};
+app.use(gzipStatic('public'));
+//app.use(express.static('public'));
 
-  io.on('connection', function (socket) {
-    users[socket.id.replace('/#', '')] = 'Unknown';
+let users = {};
 
+io.on('connection', function (socket) {
+  users[socket.id.replace('/#', '')] = 'Unknown';
+
+  io.emit('users', users);
+
+  socket.on('disconnect', function () {
+    users[socket.id.replace('/#', '')] = undefined;
     io.emit('users', users);
+  });
 
-    socket.on('disconnect', function () {
-      users[socket.id.replace('/#', '')] = undefined;
-      io.emit('users', users);
-    });
-
-    // incoming chat message
-    socket.on('chat message', function (msg) {
-      io.emit('chat message', msg);
-      var chatMsg = JSON.parse(msg);
-      chatBot.botReply(users[chatMsg.user], chatMsg.message, function (reply) {
-        var botMsg = {
-          user: 'Bot',
-          message: reply,
-          time: chatMsg.time
-        };
-        io.emit('chat message', JSON.stringify(botMsg));
-      });
-    });
-
-    // incoming key press
-    socket.on('chat isTyping', function (typing) {
-      io.emit('chat isTyping', typing);
-    });
-
-    // incoming user name change
-    socket.on('users', function (name) {
-      users[socket.id.replace('/#', '')] = name;
-      io.emit('users', users);
+  // incoming chat message
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+    var chatMsg = JSON.parse(msg);
+    chatBot.botReply(users[chatMsg.user], chatMsg.message, function (reply) {
+      var botMsg = {
+        user: 'Bot',
+        message: reply,
+        time: chatMsg.time
+      };
+      io.emit('chat message', JSON.stringify(botMsg));
     });
   });
 
-  var port = process.env.PORT || 5000;
-
-  httpServer.listen(port, function () {
-    // eslint-disable-next-line no-console
-    console.log('listening on *:5000');
+  // incoming key press
+  socket.on('chat isTyping', function (typing) {
+    io.emit('chat isTyping', typing);
   });
-};
+
+  // incoming user name change
+  socket.on('users', function (name) {
+    users[socket.id.replace('/#', '')] = name;
+    io.emit('users', users);
+  });
+});
+
+var port = process.env.PORT || 5000;
+
+httpServer.listen(port, function () {
+  // eslint-disable-next-line no-console
+  console.log('listening on *:5000');
+});
--- server/index.js
+++ server/index.js
@@ -2,10 +2,8 @@ console.log('starting app!');
 
 process.env.NODE_ENV = 'production';
 
-const socket = require('socket.io');
-
 require('babel-register')({
   presets: [ 'es2015' ]
 });
 
-require('./app')(socket);
+require('./app');
