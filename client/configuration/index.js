5fd845c1b1613bdf403102da29d1049fc48b5812
Kiran Srikanta <Kiran.Srikanta@emc.com>
2016-08-30 13:57:05 +0530

    bug fixes.

--- client/configuration/index.js
+++ client/configuration/index.js
@@ -1,6 +1,6 @@
 class Configuration {
   constructor(socket) {
-    const configStrs = (document.cookie && document.cookie.indexOf('SillyChatConfig=') > 0) ? document.cookie.split('SillyChatConfig=')[1].split('##') : ['', 'true'];
+    const configStrs = (document.cookie && document.cookie.indexOf('SillyChatConfig=') >= 0) ? document.cookie.split('SillyChatConfig=')[1].split('###') : ['', 'true'];
     this._name = configStrs[0];
     this._displayNotification = configStrs[1] == 'true';
     this._socket = socket;
@@ -22,11 +22,13 @@ class Configuration {
   }
 
   setName(name) {
+    this._name = name;
     document.cookie = `SillyChatConfig=${name}###${this._displayNotification}`;
     this._socket.emit('users', name);
   }
 
   setDisplayNotification(displayNotification) {
+    this._displayNotification = displayNotification;
     document.cookie = `SillyChatConfig=${this._name}###${displayNotification}`;
   }
 }
