28dfa41254c7ebf85dc2a9b33496a3e0be1a33de
IEUser <IEUser>
2016-11-29 15:28:23 +0530

    added testcase for email send error.

--- server/bot_services/todo.test.js
+++ server/bot_services/todo.test.js
@@ -5,28 +5,54 @@ require('babel-register')({
 });
 
 var proxyquire = require('proxyquire');
-var mockNodemailer = {
-    createTransport: function (smptConnection) {
-        return {
-            sendMail: function (mailOptions, callback) {
-                callback(null, {});
-            }
-        }
-    }
-};
-
-var TodoService = proxyquire('./todo', { 'nodemailer': mockNodemailer }).default;
-
 
 test.cb('Bot can create a reminder', t => {
+    var mockNodemailer = {
+        createTransport: function(smptConnection) {
+            return {
+                sendMail: function(mailOptions, callback) {
+                    callback(null, {});
+                }
+            }
+        }
+    };
+
+    var TodoService = proxyquire('./todo', { 'nodemailer': mockNodemailer }).default;
+
     var todoService = new TodoService();
     var responceCount = 0;
-    todoService.serve(function (resp) {
+    todoService.serve(function(resp) {
         if (responceCount == 0) {
             t.true(resp == 'OK. Adding task <u>test</u> to Remember The Milk');
             responceCount++;
         } else if (responceCount == 1) {
             t.true(resp.indexOf('Task added: see') > -1);
+            t.end();
+        }
+    }, null, ['test'])
+});
+
+test.cb('Bot can handle error when creating a reminder', t => {
+    var mockNodemailer = {
+        createTransport: function(smptConnection) {
+            return {
+                sendMail: function(mailOptions, callback) {
+                    callback(new Error('Something went wrong'), null);
+                }
+            }
+        }
+    };
+
+    var TodoService = proxyquire('./todo', { 'nodemailer': mockNodemailer }).default;
+
+    var todoService = new TodoService();
+    var responceCount = 0;
+    todoService.serve(function(resp) {
+        if (responceCount == 0) {
+            t.true(resp == 'OK. Adding task <u>test</u> to Remember The Milk');
+            responceCount++;
+        } else if (responceCount == 1) {
+            t.true(resp.indexOf('I Goofed up') > -1);
             t.end();
         }
     }, null, ['test'])
